334a65832b853d99e76a5558732be3dd632fb57c
kannan <kannan@cloudrevelinnovation.com>
2024-03-08 10:16:24 +0530

    first commit

---
+++ LoginPage/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using LoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginPage.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(LoginPage.Models.User userModel)
+        {
+            using (FirstDatabaseEntities db = new FirstDatabaseEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.password == userModel.password).FirstOrDefault();
+                if(userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Worng username or password";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["UserID"] = userDetails.UserID;
+                    Session["UserName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+              
+        }
+        public ActionResult Logout()
+        {
+            int userId = (int)Session["UserID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+
+        }
+    }
+}
